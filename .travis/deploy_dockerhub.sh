e6de8795cafa63db967808a69338b444f4bb35e3
stephentcst <neebs@live.ca>
2019-11-03 05:46:29 +0000

    step 9

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"
-else
-    TAG="$TRAVIS_BRANCH"
-fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $TRAVIS_REPO_SLUG:$TAG
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+# heroku plugins:install heroku-container-registry
+docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
+heroku container:push web --app $HEROKU_APP_NAME
+heroku container:release web --app $HEROKU_APP_NAME
